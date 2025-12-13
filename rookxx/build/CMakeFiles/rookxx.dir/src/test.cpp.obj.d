f5bc2254fa759a8bc9d123b3bb07f7b9e6b35784
Black Rook Games <BlackRookGames@gmail.com>
2025-12-12 20:31:42 -0500

    Complete overhaul

---
+++ rookxx/build/CMakeFiles/rookxx.dir/src/test.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/rookxx.dir/src/test.cpp.obj: \
+ C:\Users\zac21\BlackRookGames\github\rookpp\rookxx\src\test.cpp
--- test/main.cpp
+++ test/main.cpp
@@ -1,20 +1,7 @@
-#include <cstddef>
-#include <cstdint>
 #include <iostream>
-#include <utility>
-
-#include "ramen/String8.h"
 
 int main(int argc, char** argv)
 {
-    if (argc < 2)
-    {
-        std::cout << *argv << " <string0> [<string1> [<string2> ...]]" << std::endl;
-        return 0;
-    }
-    rookxx::ramen::String8 str;
-    for (int i = 1; i < argc; ++i)
-        str = ';' + str + argv[i];
-    std::cout << str << std::endl;
+    std::cout << "Hello World" << std::endl;
     return 0;
 }
