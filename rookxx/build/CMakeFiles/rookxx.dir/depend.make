1bbc279a77f4eb86134d1c45c59b4bd5275639da
Black Rook Games <BlackRookGames@gmail.com>
2025-12-12 20:33:05 -0500

    Add .gitignore

--- rookxx/build/CMakeFiles/rookxx.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for rookxx.
-# This may be replaced when dependencies are built.
--- rookxx/build/CMakeFiles/rookxx.dir/src/test.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/rookxx.dir/src/test.cpp.obj: \
- C:\Users\zac21\BlackRookGames\github\rookpp\rookxx\src\test.cpp
